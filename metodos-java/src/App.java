08970aebe5954c547eb89c164b0e01848ff6ed67
Mariano-JR <mariano_br123@hotmail.com>
2023-08-29 14:40:46 -0300

    Adição de conteudo sobre metodos Java

---
+++ metodos-java/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Metodos são funções ou subfunções de uma classe
+        //Por convenção, a nomeação dos Metodos são feitos sobre 2 criterios:
+        // 1 - Deve ser nomeado como verbo
+        // 2 - Seguir o padrão camelCase
+    }
+}
